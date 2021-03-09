6c2bb1664cbcf8a67e36b8d2b66cab440b28e26e
lulch <lulchevagergana@gmail.com>
2021-03-09 17:31:29 +0200

    Polymorphism

---
+++ src/LABPolymorphism/animal/Cat.java
@@ -0,0 +1,12 @@
+package LABPolymorphism.animal;
+
+public class Cat extends Animal{
+    protected Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    protected String explainSelf() {
+        return String.format ("%s%nMEEOW",super.explainSelf ());
+    }
+}
