edba94b92884c5a1dd63f3a58333eb3fbcb65003
lulch <lulchevagergana@gmail.com>
2021-04-05 17:47:29 +0300

    countryStrike exam

---
+++ src/CounterStriker/repositories/PlayerRepository.java
@@ -0,0 +1,45 @@
+package CounterStriker.repositories;
+
+import CounterStriker.models.players.Player;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import static CounterStriker.common.ExceptionMessages.INVALID_PLAYER_REPOSITORY;
+
+public class PlayerRepository implements Repository<Player> {
+    private ArrayList<Player> players;
+
+    public PlayerRepository(){
+        players = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<Player> getModels() {
+        return Collections.unmodifiableList(players);
+    }
+
+    @Override
+    public void add(Player model) {
+        if(model == null){
+            throw new NullPointerException(INVALID_PLAYER_REPOSITORY);
+        }
+        players.add(model);
+    }
+
+    @Override
+    public boolean remove(Player model) {
+        for (Player player : players) {
+            if(player == model){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Player findByName(String name) {
+        return players.stream().filter(p -> p.getUsername().equals(name)).findFirst().orElse(null);
+    }
+}
